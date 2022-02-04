04566dc90fd75bd61ed59240da88377071d0a281
Vinicius Bohrer <vbsanttos@gmail.com>
2022-02-04 20:00:46 -0300

    update callbacks to async/await

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const { readFile } = require("fs");
+const { readFile } = require("fs").promises;
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (_request, response) => {
-  readFile("./home.html", "utf-8", (err, html) => {
-    if (err) {
-      response.status(500).send("Sorry, out of order!");
-    }
-
-    response.send(html);
-  });
+app.get("/", async (_request, response) => {
+  try {
+    const html = await readFile("./home.html", "utf-8");
+    response.status(200).send(html);
+  } catch (error) {
+    response.status(500).send("Sorry, out of order!");    
+  }
 });
 
 app.listen(port, () => console.log(`Available at http://localhost:${port}/`));
